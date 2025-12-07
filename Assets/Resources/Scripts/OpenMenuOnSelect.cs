084456c41db5cfe324e93b275066dc2e3c23960e
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2025-12-07 21:24:50 +0100

    Implemented HA & OA UI

---
+++ Assets/Resources/Scripts/OpenMenuOnSelect.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class OpenMenuOnSelect : MonoBehaviour
+{
+    public Canvas menuCanvas;
+    public bool headAnchored = true;
+    public Transform objectAnchor;   // only used for cube case
+
+    public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        menuCanvas.gameObject.SetActive(true);
+
+        if (headAnchored)
+        {
+            var cam = Camera.main.transform;
+            menuCanvas.transform.SetParent(cam, false);
+            menuCanvas.transform.localPosition = new Vector3(0, 0, 0.5f);
+            menuCanvas.transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            if (objectAnchor == null) return;
+            menuCanvas.transform.SetParent(objectAnchor, false);
+            menuCanvas.transform.localPosition = Vector3.zero; // the canvas becomes a child of the objectAnchor
+            menuCanvas.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
